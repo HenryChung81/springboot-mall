73ab8072db9e001280775be06ab4977382d1a9d3
Henry Chung <chungjenhao81@gmail.com>
2022-07-07 15:06:31 +0800

    feat: add product create

--- src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.springbootmall.dao.ProductDao;
+import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 import com.example.springbootmall.service.ProductService;
 
@@ -17,5 +18,12 @@ public class ProductServiceImpl implements ProductService {
   public Product getProductById(Integer productId) {
     return productDao.getProductById(productId);
   }
+
+  @Override
+  public Integer createProduct(ProductRequest productRequest) {
+    return productDao.createProduct(productRequest);
+  }
+
+  
   
 }
