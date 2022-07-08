7d8d0d7bace77930ee95a163162b4313e6a12476
Henry Chung <chungjenhao81@gmail.com>
2022-07-08 08:16:46 +0800

    feat: add product update

--- src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
@@ -24,6 +24,16 @@ public class ProductServiceImpl implements ProductService {
     return productDao.createProduct(productRequest);
   }
 
+  @Override
+  public void updateProduct(Integer productId, ProductRequest productRequest) {
+    productDao.updateProduct(productId, productRequest);
+    
+  }
+
+  
+
+
+
   
   
 }
