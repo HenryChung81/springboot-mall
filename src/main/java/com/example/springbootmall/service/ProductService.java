2044db16e63e3d4dc66f480a49980fa1dc9b6236
Henry Chung <chungjenhao81@gmail.com>
2022-07-10 18:37:30 +0800

    feat: add productquery by category & search

--- src/main/java/com/example/springbootmall/service/ProductService.java
+++ src/main/java/com/example/springbootmall/service/ProductService.java
@@ -2,12 +2,13 @@ package com.example.springbootmall.service;
 
 import java.util.List;
 
+import com.example.springbootmall.constant.ProductCategory;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
 public interface ProductService {
 
-  List<Product> getProducts();
+  List<Product> getProducts(ProductCategory category, String search);
   
   Product getProductById(Integer productId);
 
--- src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.springbootmall.constant.ProductCategory;
 import com.example.springbootmall.dao.ProductDao;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
@@ -17,8 +18,8 @@ public class ProductServiceImpl implements ProductService {
   private ProductDao productDao;
 
   @Override
-  public List<Product> getProducts() {
-    return productDao.getProducts();
+  public List<Product> getProducts(ProductCategory category, String search) {
+    return productDao.getProducts(category, search);
   }
 
   @Override
