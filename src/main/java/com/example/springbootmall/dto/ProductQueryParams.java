c11cba2a8a1c3230efd7955530f9413f6c4ac35e
Henry Chung <chungjenhao81@gmail.com>
2022-07-11 14:19:30 +0800

    feat: add prodcut query orderBy & sort

--- src/main/java/com/example/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/example/springbootmall/dto/ProductQueryParams.java
@@ -6,7 +6,9 @@ public class ProductQueryParams {
   
   private ProductCategory category;
   private String search;
-  
+  private String orderBy;
+  private String sort;
+
   public ProductCategory getCategory() {
     return category;
   }
@@ -16,9 +18,24 @@ public class ProductQueryParams {
   public String getSearch() {
     return search;
   }
+
   public void setSearch(String search) {
     this.search = search;
   }
+  public String getOrderBy() {
+    return orderBy;
+  }
+  public void setOrderBy(String orderBy) {
+    this.orderBy = orderBy;
+  }
+  public String getSort() {
+    return sort;
+  }
+  public void setSort(String sort) {
+    this.sort = sort;
+  }
+  
+  
 
   
 }
