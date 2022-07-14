31dc75bd90b80450b8b93e7b952269e084ac7ce7
Henry Chung <chungjenhao81@gmail.com>
2022-07-14 16:05:25 +0800

    feat: add user check email & hide password

--- src/main/java/com/example/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/UserServiceImpl.java
@@ -1,14 +1,21 @@
 package com.example.springbootmall.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.example.springbootmall.dao.UserDao;
 import com.example.springbootmall.dto.UserRegisterRequest;
 import com.example.springbootmall.model.User;
 import com.example.springbootmall.service.UserService;
 
 @Component
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+  
+  private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
   
   @Autowired
   private UserDao userDao;
@@ -20,7 +27,17 @@ public class UserServiceImpl implements UserService{
 
   @Override
   public Integer register(UserRegisterRequest userRegisterRequest) {
-    return userDao.createUser(userRegisterRequest);
+    // check registered email
+    User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+    if (user != null) {
+      log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    } else {
+
+      // create a new account
+      return userDao.createUser(userRegisterRequest);
+    }
   }
   
 }
